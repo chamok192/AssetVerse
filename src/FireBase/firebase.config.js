ff7aca05935cc1820926419aaa1b56669f43a48b
Chamok Bhadra <chamokbhadra@gmail.com>
2025-12-10 00:24:18 +0600

    installing firebase and add firebase file

---
+++ src/FireBase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAsNZaDN6Y5S5maXPXwVC0qx8ZcuPe8sKM",
+    authDomain: "assetverse-2121.firebaseapp.com",
+    projectId: "assetverse-2121",
+    storageBucket: "assetverse-2121.firebasestorage.app",
+    messagingSenderId: "538514865576",
+    appId: "1:538514865576:web:544d34aebf83949ce3f19d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
